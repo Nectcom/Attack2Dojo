06616420de62011d8fc5180c555fae835811368a
Nectcom <lbwh6840@yahoo.co.jp>
2017-09-02 17:59:31 +0900

    aa

--- www/javascripts/app.js
+++ www/javascripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ['onsen']);
 
 // what u want? -> toppage.htmlのinputに入力された値をpwdとしてurlに投げ、取得したページをpage1.htmlに出す
 app.value('pwds', {
-  'login_pass' : {'pwd' : 2384},
+  'login_pass' : {'pwd' : 3770},
   'username': 'crapcrap',
   'userpwd': 'thisistestid',
 });
@@ -13,97 +13,129 @@ app.value('urls', {
   'userLogin' : ['POST' , 'https://library.rail-e.or.jp/e-library/Login']
 });
 
-/**
- * HTTP通信を制御する
- * */
-app.factory('HTTPRequestService', ['$http', '$q', 'urls', '$httpParamSerializerJQLike', 
-  function($http, $q, urls, $httpParamSerializerJQLike){
-    var httpRequestService = {};
-    var deferred = $q.defer();
-    var promise = deferred.promise;
+
+app.service('LoginService', ['$http', '$q', '$httpParamSerializer', 
+  function($http, $q, $httpParamSerializer){
+    /**
+     * 道場パスとユーザーログインを一度に行う
+     * 
+     * @param bookPass 雑誌掲載のパスワード
+     * @param username
+     * @param userPass ユーザーログイン用のパスワード
+     */
+    this.login = function(bookPass, username, userPass){
+      var responceText = '';
+      var self = this;
+      var phase1 = this.htmlRequest('POST', 'https://library.rail-e.or.jp/e-library/Index', {'pwd': bookPass});
+      var phase2 = phase1.then(function onSuccess(responce){
+          // Error check.
+          if(responce.data.match(/TopError/)){
+            console.error('Phase1 Responce Error');
+            responceText = responce.data;
+            return $q.reject('Phase1 Responce Error');
+          }
+          
+          return self.htmlRequest('GET', 'https://library.rail-e.or.jp/e-library/jsp/dojo/index.jsp');
+        }, function onError(responce){
+          console.error('Phase1 HTTP Error.');
+      });
+      
+      var phase3 = phase2.then(function onSuccess(responce){
+          var userForm = new FormData();
+          userForm.append('act', 'login');
+          userForm.append('textPersonID', username);
+          userForm.append('passwordPassword', userPass);
+          userForm.append('textPersonID_hid', getEncodeElementValue(username));
+          userForm.append('passwordPassword_hid', getEncodeElementValue(userPass));
+          
+          return self.htmlRequest('POST', 'https://library.rail-e.or.jp/e-library/Login', 
+            {'act' : 'login',
+            'textPersonID': username,
+            'passwordPassword' : userPass,
+            'textPersonID_hid' : getEncodeElementValue(username),
+            'passwordPassword_hid' : getEncodeElementValue(userPass),
+            'checkRemember' : ''
+            });
+          
+          // return $http({ method : 'POST',
+          //   headers : { 'Content-Type' : 'application/x-www-form-urlencoded' },
+          //   url : 'https://library.rail-e.or.jp/e-library/Login',
+          //   data : userForm
+          // });
+        }, function onError(responce){
+          console.error('Phase2 HTTP Error.');
+      });
+        
+      phase3.then(function onSuccess(responce){
+        // Error check.
+        if(responce.data.match(/ログインエラー/)){
+          console.error('Phase3 Responce Error.');
+          responceText = responce.data;
+          return $q.reject;
+        }
+        responceText = responce.data;
+      }, function onError(responce){
+        console.error('Phase3 HTTP Error.');
+      });
+      
+      // Complete session.
+      $q.all([phase1, phase2, phase3]).then(function(){
+        console.log('logged in.');
+        console.log(responceText);
+      },
+      function(){
+        console.log(responceText);
+        return responceText;
+      });
+      
+    };
+    
+    /**
+     * HTTPリクを送信してその$qを返す
+     * 
+     * @param type
+     * @param url
+     * @pram param
+     * 
+     * @return http Q object
+     */
+    this.htmlRequest = function(type, url, param){
+      return $http({
+        method : type,
+        headers : { 'Content-Type' : 'application/x-www-form-urlencoded; charset=Shift-JIS' },
+        url : url,
+        data : $httpParamSerializer(param)
+      });
+    };
 
     /**
-     * 指定されたページを取得する
-     * @param add_key urls[add_key]でRESTとURLを指定する
-     * @param datas 同時に送信するデータ
-     * @callback callback
-     * */
-    this.getHtml = function(add_key, datas, callback){
-      // console.debug('request ' + urls[add_key][1]);
-      $http({
-        method : urls[add_key][0],
-        headers : {
-        'Content-Type' : 'application/x-www-form-urlencoded'
-        },
-        transformRequest : $httpParamSerializerJQLike,
-        url : urls[add_key][1],
-        data : datas
-      }).then(function onSuccess(response){
-        callback(response.data);
-      }, function onError(response){
-        console.log('error : ' + urls[add_key][1]);
-      });
-    };
-    
-    /**
-     * 実力試験道場へログインし、成功判定を行う
-     * @param bookcode 会誌のパスコード
-     * @param username
-     * @param userpass
-     * */
-    this.userLogin = function(bookcode, username, userpass){
-      
-      promise
-      .then(function(bookcode){
-        getHtml('index', ['pwd', bookcode], function(data){
-          // Error check.
-          if(data.match(/TopError/)){
-            deferred.reject('booklogin');
-          }else{
-            deferred.resolve();
-          }
-        });
-      })
-      .then(userLogin(username,userpass));
-      
-      return promise;
-    };
-    
-    /**
-     * 2nd. User Login
-     * @param username
-     * @param userpass
-     * */
-    var userLogin = function(username, userpass){
-      getHtml('login', {}, function(data){
-        var loginParams = {
-          'act' : 'login',
-          'textPersonID' : username,
-          'passwordPassword' : userpass,
-          'textPersonID_hid' : getEncodeElementValue(username),
-          'passwordPassword_hid' : getEncodeElementValue(userpass)
-        };
-        
-        getHtml('userLogin', loginParams, function(data){
-          //TODO: error check
-          if(data.match(/ログインエラー/)){
-            return false;
-          }else{
-            return true;
-          }
-        });
-        
-      });
-      
-    };
-    return httpRequestService;
-  }]);
-      
+     * Common.jpよりコピー
+     * まさかの(簡易式)自己暗号化
+     * 
+     * @param elementValue 暗号化したい値
+     * @return 暗号化後の値
+     */
+    this.getEncodeElementValue = function(elementValue){
+		  var elementValueBefChg = elementValue;
+      var elementValueAftChg = "";
+      for(i=0;i<elementValueBefChg.length;i++){
+        var eachValue = elementValueBefChg.charCodeAt(i);
+        var eachEncodeValue = eachValue-7;
+        if(i!=0){
+          elementValueAftChg=elementValueAftChg+","+eachEncodeValue;
+        }else{
+          elementValueAftChg=eachEncodeValue;
+        }  
+      }
+      return elementValueAftChg;
+	  };
+}]);
+
 /**
  * @classdesc アプリ全体のページ遷移を管理する
  * */
-app.controller('PageController', ['$scope', 'pwds', 'urls', 'HTTPRequestService', 
-  function($scope, pwds, urls, httpRequestService){
+app.controller('PageController', ['$scope', 'pwds', 'urls', 'LoginService', 
+  function($scope, pwds, urls, LoginService){
     $scope.pwd = pwds['login_pass']['pwd'];
     $scope.username = pwds['username'];
     $scope.userpwd = pwds['userpwd'];
@@ -113,50 +145,9 @@ app.controller('PageController', ['$scope', 'pwds', 'urls', 'HTTPRequestService'
     $scope.firstLogin = function(){
       console.debug('go login.');
       $scope.isSpin = true;
-
-      // Fail pattern.
-      // httpRequestService.getHtml('index', {}, function(data){
-      httpRequestService.getHtml('index', pwds['login_pass'], function(data){
-
-        // Error Handring.
-        if(data.match(/TopError/)){
-          console.log("Password missed");
-          alert('実力試験道場のパスワードが間違っています');
-          $scope.isSpin = false;
-          return;
-        }
-
-        httpRequestService.getHtml('login', {}, function(data){
-          // console.debug($(data).find('.footer').text());
-          var loginQuery = $(data);
-          
-          loginQuery.find('.login_input01 > #textPersonId').val($scope.username);
-          loginQuery.find('#passwordPassword').val($scope.userpwd);
-
-          // Do Login submit.
-          var personID = loginQuery.find('#textPersonId').val();
-          var password = loginQuery.find('#passwordPassword').val();
-          
-          loginQuery.find('#textPersonId_hid').val(getEncodeElementValue(personID));
-          loginQuery.find('#passwordPassword_hid').val(getEncodeElementValue(password));
-          loginQuery.find('input[name=act]').val('input');
-
-         var params = { 'act' : 'login',
-                        'textPersonId' : personID, 'passwordPassword' : password,
-                        'textPersonId_hid' : getEncodeElementValue(personID),
-                        'passwordPassword_hid' : getEncodeElementValue(password)
-                       };
-
-          httpRequestService.getHtml('userLogin', params, makeFiledList(data));
-
-          $scope.app.slidingMenu.setMainPage('page1.html');
-        });
-      });
-
-      // make main page test list.
-      // @params data return html data
-      function makeFieldList(data){
-        
-      }
-    }
+      myNavigator.pushPage('page1.html');
+      
+      var responceText = LoginService.login($scope.pwd, $scope.username, $scope.userpwd);
+      // $scope.debugdata = responceText;  
+    };
 }]);
